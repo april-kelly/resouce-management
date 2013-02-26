dc1fd043507d0852e69a9a28bfbccbcc7891f82a
root <root@liam-Inspiron-1545.(none)>
2013-02-26 10:34:19 -0700

    Added a stub for settings.

---
+++ liam/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+	Name: Settings
+	Description: Allows the user to change settings for the resource tracker
+	Programmer: Liam Kelly
+	Last Modified: 02/26/13	
+*/
+
+//This file is currently a stub
+
+?>
