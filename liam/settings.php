b7ac5a77e10902312b0719871c5097b92c6ab9b4
root <root@liam-Inspiron-1545.(none)>
2013-02-26 12:32:29 -0700

    Figure out how to add up hours by resource by week.

--- liam/settings.php
+++ liam/settings.php
@@ -5,8 +5,10 @@
 	Description: Allows the user to change settings for the resource tracker
 	Programmer: Liam Kelly
 	Last Modified: 02/26/13	
+	Comments: This file is currently a stub.
 */
 
-//This file is currently a stub
+//include the data object
+include('data.php');
 
 ?>
--- liam/test.php
+++ liam/test.php
@@ -2,9 +2,82 @@
 //include the data object
 include('data.php');
 
-//fetch the test table
-$results = query('SELECT * FROM test');
+//Connect to the database
+$dbc = new db;
+$dbc->connect();
+	
 
+//Fetch the list of resources
+$people = $dbc->query('SELECT * FROM people');	//Get the table of people
+
+foreach($people as $people){
+	
+	$project = $dbc->query("SELECT * FROM test WHERE resource='".$people['index']."' ");	//Get the table of people
+	
+	$past_week = '';
+	$i = '0';
+	$sort = $project;
+	$hours = '';
+	if(!(empty($project))){
+	//echo out the results
+	foreach($project as $project)
+	{
+		//Determine if the week of: label and table header should be spit out
+		if(!($past_week == $project['week_of'])){
+			echo '<br /><b>Week of: '.$project['week_of'].'</b><br />';
+			echo 'Resource: '.$people['name'].'<br />';
+		
+			//Process the hours
+		
+				//unserialize the hours array
+				$time = unserialize($project['time']);
+		
+				//add everything up
+				$hours = $hours + $time['sunday']
+						+ $time['monday']
+						+ $time['tuesday']
+						+ $time['wednesday']
+						+ $time['thursday']
+						+ $time['friday']
+						+ $time['saturday'];
+			
+		
+			//Add one to the increment operator
+			$j = $i + 1;
+			
+			//See if another record exists
+			if(array_key_exists($j, $sort)){
+			
+				//If another record exists see if it is of the same week as the current record
+				if(!($project['week_of'] == $sort[$j]['week_of'])){
+					echo '</tr><table>';
+				}
+			
+			}else{
+				echo '</tr><table>';
+			}
+		}
+		
+		//echo out the hours
+		echo 'Hours: '.$hours.'<br />';
+		
+		//empty the hours variable
+		$hours = '';
+			
+		//Make the past week equal this week before moving to the next record
+		$past_week = $project['week_of'];
+		$i++;
+		
+	}
+	}
+
+
+	
 }
-
+	
+	
+	
+//close the database connection
+$dbc->close();
+	
 ?>
