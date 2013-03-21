12f3382afdc2bc601efd2328e4f34c2106b69d99
Liam Kelly <liam@bluetent.com>
2013-03-21 11:43:44 -0600

    week.php is now fixed.

--- liam/week.php
+++ liam/week.php
@@ -37,33 +37,196 @@ for($i = 2; $i <= $show; $i++){
 $count = count($weeks);
 
 //variables
-$person = 1;
 $past_week = '';
+$week = $weeks; //create a week of weeks
 
 //Database connection
 $dbc = new db;
 
 $dbc->connect();
 
-$people = $dbc->query('SELECT * FROM people');
-$projects = $dbc->query('SELECT * FROM `jobs` WHERE resource = '.$person.' ');
+$people		 = $dbc->query('SELECT * FROM people');
+$person 	 = $dbc->sanitize($_REQUEST['p']);
+$projects        = $dbc->query('SELECT * FROM `jobs` WHERE resource = '.$person.' ');
 
-foreach($projects as $projects){
+$test = $person - 1;
+
+echo "<h3>Weekly expanded view for ".$people[$person]['name']."</h3>";
+
+//sort by week
+foreach($week as $week){
+
+	$project = $projects; //reset the destroyed project variable
 	
-	//create a copy of weeks
-	$copy = $weeks;
+	//sort by project
 	
-	foreach($copy as $copy){
-		echo $projects['week_of'].'  '.$past_week."\r\n";
-		//if the week exists in both $copy and $projects echo out the project
-		if($projects['week_of'] == $copy/* && $projects['week_of'] !== $past_week*/){
+	if(!(empty($project))){
+	foreach($project as $project){
+		
+		//echo out project if found
+		if($project['week_of'] == $week){
+			
+			if($past_week !== $project['week_of']){
+			if($past_week !== ''){ echo '</table><br /><br />'; }
+			echo $week;
+			?>
+			 <table border="1">
+			 <tr>
+			  <td>Resource:</td>
+			  <td>Manager</td>
+			  <td>Project id:</td>
+			  <td>Priority</td>
+			  <td>Sunday</td>
+			  <td>Monday</td>
+			  <td>Tuesday</td>
+			  <td>Wednesday</td>
+		 	  <td>Thursday</td>
+		 	  <td>Friday</td>
+		 	  <td>Saturday</td>
+		 	  <td>Sales Status</td>
+		 	 </tr>
+		 	<?php
+		 	}
+		
+		
+		//make boolean sales_status human readable
+		if($project['sales_status'] == '0'){
+			$status = "Opportunity";
+		}else{
+			$status = "Sold";
+		}
+		
+		//make numeric priority human readable
+		if($project['priority'] == '0'){
+			$priority = "Very High";
+		}elseif($project['priority'] == '1'){
+			$priority = "High";
+		}elseif($project['priority'] == '2'){
+			$priority = "Medium";
+		}elseif($project['priority'] == '3'){
+			$priority = "Low";
+		}
+                
+		//Verify project manager exists
+		if(isset($people[$project['manager']])){
+			$manager = true;
+		}else{
+			$manager = false;
+		}
+		
+		//Verify resource exists
+		if(isset($people[$project['resource']])){
+			$resource = true;
+		}else{
+			$resource = false;
+		}
+		
+			echo '<tr/>';
+			//echo out the resource
+			if($resource == true){
+				echo '<td>',$people[$project['resource']]['name'],'</td>';
+			}else{
+				echo '<td><span style="color: red;">[Error]</span></td>';
+			}
+			
+			//echo out manager
+			if($manager == true){
+				echo '<td>',$people[$project['manager']]['name'],'</td>';
+			}else{
+				echo '<td><span style="color: red;">[Error]</span></td>';
+			}
+			
+		 	//Unserialize the time
+		 	$time = unserialize($project['time']);
+		 	
+		 	//echo out the rest of the table
+			echo '<td>',$project['project_id'],'</td>';
+			echo '<td>',$priority,'</td>';
+			echo '<td>',$time['sunday'],'</td>';
+			echo '<td>',$time['monday'],'</td>';
+			echo '<td>',$time['tuesday'],'</td>';
+			echo '<td>',$time['wednesday'],'</td>';
+			echo '<td>',$time['thursday'],'</td>';
+			echo '<td>',$time['friday'],'</td>';
+			echo '<td>',$time['saturday'],'</td>';
+			echo '<td>',$status,'</td>';
+			
+			echo '</tr>';
+			
+			$past_week = $week;
+			
+		}
+	}
+	
+	}else{
+		if($past_week == ''){ echo "<h3>Sorry, no records were found</h3>"; }
+		$past_week = $week;
+	}
+	
+}
+
+
+//Close database connection
+$dbc->close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+echo $projects['week_of'].'  '.$past_week."\r\n";
+		//if the week exists in both $week and $projects echo out the project
+		if($projects['week_of'] == $week/* && $projects['week_of'] !== $past_week){
 			
 			//echo out the table header
 			if(!($projects['week_of'] == $past_week)){ 
 				echo "equal";
 							if($past_week !== ''){ echo '</table>'; }
 			?>
-			 <br /><?php echo $copy; ?>
+			 <br /><?php echo $week; ?>
 			 <table border="1">
 			 <tr>
 			  <td>Resource:</td>
@@ -83,7 +246,7 @@ foreach($projects as $projects){
 		 	}
 		
 		
-		///*//make boolean sales_status human readable
+		//make boolean sales_status human readable
 		if($projects['sales_status'] == '0'){
 			$status = "Opportunity";
 		}else{
@@ -135,23 +298,12 @@ foreach($projects as $projects){
 			echo '<td>',$time['thursday'],'</td>';
 			echo '<td>',$time['friday'],'</td>';
 			echo '<td>',$time['saturday'],'</td>';
-			echo '<td>',$status,'</td>';//*/
+			echo '<td>',$status,'</td>';
 			
 		 	
 		 	//set the past week
 			$past_week = $projects['week_of'];
-			
-		}
-		
-	}
-
-}
-
-
-//Close database connection
-$dbc->close();
-
-
+			*/
 
 /*	
 	//connection for projects
