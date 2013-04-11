4d595a093fd8d598d02b04a55a7320859338fc03
Liam Kelly <liam@bluetent.com>
2013-04-11 12:27:49 -0600

    Continued minor bug fixes.

--- liam/insert.php
+++ liam/insert.php
@@ -3,25 +3,70 @@
 /*
 	Database Insert Script
 	Programmer: Liam Kelly
-	Last Modified: 2/22/2013
+	Last Modified: 4/09/2013
 */
 
 //Include the data object
 include('data.php');
 
-//Preset variables:
-$week_of = $_REQUEST['start_date'];  //rename to week_of upon completion of debugging
-
 //Debugging Flags:
-$debug = false;		//flag to prevent running query					Default: false
-$valid = true;		//flag to prevent user data validation				Default: true
-$sanitize = true;	//flag to prevent user data sanitation (use with caution)	Default: true
+$debug = true;		//flag to prevent running query					Default: false
+$valid = true;		//flag to enable user data validation				Default: true
+$sanitize = true;	//flag to enable user data sanitation (use with caution)	Default: true
 $fail = false;		//flag to terminate the insert if something fails		Default: false
 
+//convert date(s) to Y-m-d format
+$week_of = date("Y-m-d", strtotime($week_of));
+                                                        
+//connect to the database	
+$dbc = new db;
+$dbc->connect();
 
-//check for invalid user inputs
-if($valid == false){
-		
+//Verifiy the user filled out all inputs correctly
+if($valid == true){
+	if(!(isset($_REQUEST['project_id']))){ $fail = true; }
+	if(!(isset($_REQUEST['manager']))){ $fail = true; }
+	if(!(isset($_REQUEST['start_date']))){ $fail = true; }
+	if(!(isset($_REQUEST['time']))){ $fail = true; }
+	if(!(isset($_REQUEST['resource']))){ $fail = true; }
+	if(!(isset($_REQUEST['sales_status']))){ $fail = true; }
+}
+
+//sanitize the user inputs
+if($sanitize == true){
+	
+        $week_of                = $dbc->sanitize($_REQUEST['start_date']);
+	$project_id   		= $dbc->sanitize($_REQUEST['project_id']);
+	$manager     		= $dbc->sanitize($_REQUEST['manager']);
+	$resource    		= $dbc->sanitize($_REQUEST['resource']);
+	$sales_status 		= $dbc->sanitize($_REQUEST['sales_status']);
+	$priority     		= $dbc->sanitize($_REQUEST['priority']);
+	
+	//sanitize, fill and serialize the hours array
+	$hours = serialize(array( 
+		"sunday"    	=> $dbc->sanitize($_REQUEST['sunday']),
+		"monday"   	=> $dbc->sanitize($_REQUEST['monday']),
+		"tuesday"   	=> $dbc->sanitize($_REQUEST['tuesday']),
+		"wednesday" 	=> $dbc->sanitize($_REQUEST['wednesday']),
+		"thursday"  	=> $dbc->sanitize($_REQUEST['thursday']),
+		"friday"    	=> $dbc->sanitize($_REQUEST['friday']),
+		"saturday"  	=> $dbc->sanitize($_REQUEST['saturday']),
+		));
+}
+
+//If the week_of is not in Y-m-d format fix it
+$week_of = date('Ymd', $week_of)
+
+//verifiy the resource and manger requested exists
+if($valid == true){
+	if(!(verify('people', 'index', $_REQUEST['resource']))){ $fail = true; }
+	if(!(verify('people', 'index', $_REQUEST['manager']))){ $fail = true; }
+}
+
+
+//check for in valid user inputs
+if($valid == true){
+	echo "entered vaildation mode";
 	//make the sales_status be boolean
 	$sales_status = 1;
 	if($_REQUEST['sales_status'] == '1'){
@@ -58,63 +103,21 @@ if($valid == false){
 	}
 	
 	//check for an empty start date
-	if($start_date == ''){
+	if($week_of == ''){
 		header("Location: ./index.php?&nodate");
 	}
 	
+        //ensure the start date is a sunday
+        if(!(date('w', strtotime($week_of)) == '0')){
+            header("Location: ./index.php?&weekstart");
+        }
+        
 	//check for an empty priority
 	if(!(is_numeric($_REQUEST['priority'])) && 
 	   !(strlen($_REQUEST['priority']) >= '1' )){
 		header("Location: ./index.php?&priority");
 	}
-	
-}
-
-//convert date(s) to Y-m-d format
-$week_of = date("Y-m-d", strtotime($week_of));
-                                                  
-
-      
-//connect to the database	
-$dbc = new db;
-$dbc->connect();
-
-
-//sanitize the user inputs
-if($sanitize == true){
-	
-	$project_id   		= $dbc->sanitize($_REQUEST['project_id']);
-	$manager     		= $dbc->sanitize($_REQUEST['manager']);
-	$resource    		= $dbc->sanitize($_REQUEST['resource']);
-	$sales_status 		= $dbc->sanitize($_REQUEST['sales_status']);
-	$priority     		= $dbc->sanitize($_REQUEST['priority']);
-	
-	//sanitize, fill and serialize the hours array
-	$hours = serialize(array( 
-		"sunday"    	=> $dbc->sanitize($_REQUEST['sunday']),
-		"monday"   	=> $dbc->sanitize($_REQUEST['monday']),
-		"tuesday"   	=> $dbc->sanitize($_REQUEST['tuesday']),
-		"wednesday" 	=> $dbc->sanitize($_REQUEST['wednesday']),
-		"thursday"  	=> $dbc->sanitize($_REQUEST['thursday']),
-		"friday"    	=> $dbc->sanitize($_REQUEST['friday']),
-		"saturday"  	=> $dbc->sanitize($_REQUEST['saturday']),
-		));
-}
-
-//verifiy the resource and manger requested exists
-if($valid == false){
-	if(!(verify('people', 'index', $_REQUEST['resource']))){ $fail = true; }
-	if(!(verify('people', 'index', $_REQUEST['manager']))){ $fail = true; }
-}
-
-//Verifiy the user filled out all inputs correctly
-if($valid == false){
-	if(!(isset($_REQUEST['project_id']))){ $fail = true; }
-	if(!(isset($_REQUEST['manager']))){ $fail = true; }
-	if(!(isset($_REQUEST['start_date']))){ $fail = true; }
-	if(!(isset($_REQUEST['time']))){ $fail = true; }
-	if(!(isset($_REQUEST['resource']))){ $fail = true; }
-	if(!(isset($_REQUEST['sales_status']))){ $fail = true; }
+        
 }
 
 //Query(new)
@@ -154,7 +157,7 @@ if(isset($_REQUEST['debug']) || $fail == true || $debug == true){
   echo '<b>Query:</b><br />'.$query.'<hr>';
   echo '<b>Flags:</b><br />Fail: '.$fail.'<br />Debug: '.$debug.'<br />Valid: '.$valid.'<br />Sanitize: '.$sanitize.'<hr />';
   echo '<b>Values:</b><br />project_id: '.$project_id.'<br />manager: '.$manager.'<br />week_of: '.$resource.'<br />hours: '.$week_of.'<br />priority: '.$priority.'<br />sales_status: '.$sales_status.'<hr />';
-  echo '<p style="text-align: center;"><a href="./week.php" style="text-align:right;">Click Here to Continue >></a><br />(C) Copyright 2013 Liam Kelly</p>'; 
+  echo '<p style="text-align: center;"><a href="./dashboard.php" style="text-align:right;">Click Here to Continue >></a></p>'; 
  }                           
  
  //Echo out an error message if the user tries to enter debugging mode with out setting the flag first               
@@ -169,7 +172,7 @@ if(isset($_REQUEST['debug']) || $fail == true || $debug == true){
   echo '<b>Query:</b><br />'.$query.'<hr>';
   echo '<b>Flags:</b><br />Fail: '.$fail.'<br />Debug: '.$debug.'<br />Valid: '.$valid.'<br />Sanitize: '.$sanitize.'<hr />';
   echo '<b>Values:</b><br />project_id: '.$project_id.'<br />manager: '.$manager.'<br />week_of: '.$resource.'<br />hours: '.$week_of.'<br />priority: '.$priority.'<br />sales_status: '.$sales_status.'<hr />';
-  echo '<p style="text-align: center;"><a href="./month.php" style="text-align:right;">Click Here to Continue >></a><br />(C) Copyright 2013 Liam Kelly</p>'; 
+  echo '<p style="text-align: center;"><a href="./dashboard.php" style="text-align:right;">Click Here to Continue >></a><br />(C) Copyright 2013 Liam Kelly</p>'; 
  }
 
 //otherwise redirect the user to the results page
