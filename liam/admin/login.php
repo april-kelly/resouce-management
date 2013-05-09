2a78d8ee9f8fcefc43a49917ac84cf761e59ceca
Liam Kelly <liam@bluetent.com>
2013-05-09 13:16:10 -0600

    started work on login.php

--- liam/admin/login.php
+++ liam/admin/login.php
@@ -1,1 +1,13 @@
 <?php
+/*
+ * Name: Admin Login
+ * Programmer: Liam Kelly
+ * Date: 5/9/13
+ */
+
+//includes
+include('data.php');
+
+//get the users login
+$user = $_REQUEST['username'];
+$pass = $_REQUEST['password'];
