2d50f11bec6cb59c71bc5539539c4926dfecebbd
Liam Kelly <liam@bluetent.com>
2013-07-29 14:21:18 -0600

    Fixed broken link on beta.php.

--- includes/errors/beta.php
+++ includes/errors/beta.php
@@ -6,5 +6,5 @@ $_SESSION['beta'] = TRUE;
 <h3>Alert: This is a non-production version</h3>
 <p>
     The version of this software is a developmental version, not all features may be fully functional and things may still be buggy.
-    Click <a href="./p=home">here</a> to continue.
+    Click <a href="./?p=home">here</a> to continue.
 </p>
