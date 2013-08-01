16e0132a2d51d198343dba892502c40d516d4534
Liam Kelly <liam@bluetent.com>
2013-08-01 10:12:39 -0600

    Update database setup script for the addition of lastname and firstname fields.

--- includes/install/create_db.php
+++ includes/install/create_db.php
@@ -33,16 +33,19 @@ function create_db($name){
     $dbc->direct('CREATE DATABASE '.$name);
 
     //The people table
-    $table_people = 'CREATE TABLE IF NOT EXISTS `'.$name.'`.`people` (
-      `index` int(11) NOT NULL AUTO_INCREMENT,
-      `name` text NOT NULL,
-      `email` text NOT NULL,
-      `password` text NOT NULL,
-      `type` int(11) NOT NULL DEFAULT \'0\',
-      `admin` int(1) NOT NULL,
-      `reset_code` text NOT NULL,
-      PRIMARY KEY (`index`)
-    ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0
+    $table_people = 'CREATE TABLE IF NOT EXISTS `people` (
+    `index` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` char(128) NOT NULL,
+  `profile_pic` varchar(255) NOT NULL,
+  `type` int(2) NOT NULL DEFAULT \'0\',
+  `admin` int(1) NOT NULL,
+  `security_class` tinyint(4) NOT NULL DEFAULT \'0\',
+  `reset_code` text NOT NULL,
+  PRIMARY KEY (`index`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1
     ';
 
     //The jobs table
