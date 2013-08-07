7a6654be42b3fd7b1e2fee012ffaba531cac8c6d
Liam Kelly <liam@bluetent.com>
2013-08-07 14:42:38 -0600

    Updated reset script to be able to rebuild the settings.php file.

--- includes/config/reset.php
+++ includes/config/reset.php
@@ -8,6 +8,19 @@ include_once('../../path.php');
 include_once(ABSPATH.'includes/config/settings.php');
 
 $set = new settings;
+
+$rebuild = array();
+$rebuild['db_host']         = 'localhost';
+$rebuild['db_user']         = 'root';
+$rebuild['db_pass']         = 'kd0hdf';
+$rebuild['db_database']     = 'resources';
+$rebuild['server_domain']   = 'localhost';
+$rebuild['server_dir']      = '/resouce-management/';
+
+//rebuilds the settings.php file
+//$set->rebuild($rebuild);
+
+//Recreates the settings.json file
 $set->create();
 
 echo "Attempted settings rebuild/reset!";
